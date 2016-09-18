ce50d20b94fa3503e59fee8eba8033106fcdf659
Keane O'Kelley <keane.m.okelley@gmail.com>
2016-09-18 03:54:00 -0500

    Initial commit

---
+++ chores.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+import datetime
+import json
+import requests
+from apscheduler.schedulers.background import BackgroundScheduler
+import random
+import time
+import yaml
+
+SLACK_URL = 'https://hooks.slack.com/services/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'
+USERS = ('gemanley', 'jpbush', 'keane',)
+SHARED_CATEGORIES = ('Kitchen', 'General')
+
+chores = {}
+
+def get_week():
+    return datetime.datetime.now().isocalendar()[1]
+
+
+def should_run_bi_weekly():
+    return get_week() % 2 == 1
+
+
+def should_run_quad_weekly():
+    return get_week() % 4 == 1
+
+
+def post_to_slack(message):
+    requests.post(SLACK_URL, data=json.dumps(message))
+
+
+def parse_for_slack(name, user_chores):
+    for user in user_chores:
+        data = {
+           'text': '{} chores for @{}'.format(name, user),
+            'attachments': [],
+        }
+        for chore in user_chores[user]:
+            data['attachments'].append({
+                'title': chore,
+                'attachment_type': 'default',
+                'actions':  [
+                    {
+                        'name': 'done',
+                        'text': 'Done',
+                        'type': 'button',
+                        'value': 'done',
+                    },
+                ],
+            })
+        post_to_slack(data)
+
+
+def get_user_chores(chores):
+    user_chores = {}
+    for user in USERS:
+        user_chores[user] = []
+    for group in chores:
+        if group not in SHARED_CATEGORIES:
+            for user in USERS:
+                user_chores[user] += chores[group]
+        else:
+            for chore in chores[group]:
+                user_chores[random.choice(USERS)].append(chore)
+    return user_chores
+
+
+def get_chores(period):
+    chores_dict = {}
+    for group in chores[period]:
+        chores_dict.update(group)
+    return chores_dict
+
+
+def bi_weekly_clean():
+    parse_for_slack('Bi-weekly', get_user_chores(get_chores('bi-weekly')))
+
+
+def weekly_clean():
+    parse_for_slack('Weekly', get_user_chores(get_chores('weekly')))
+
+
+if __name__ == '__main__':
+    f = open('chores.yml')
+    chores.update(yaml.load(f))
+
+    bi_weekly_clean()
+    """
+    sched = BackgroundScheduler()
+    sched.start()
+    sched.add_job(bi_weekly_clean, trigger='cron', day='6')
+    sched.add_job(weekly_clean, trigger='cron', day='6')
+    """
