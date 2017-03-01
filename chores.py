c445e670e0961f29ff3038547dd01b61efc50571
Keane O'Kelley <keane.m.okelley@gmail.com>
2017-02-28 23:21:00 -0600

    Prepare for GitHub

--- chores.py
+++ chores.py
@@ -1,39 +1,25 @@
 #!/usr/bin/env python3
 # coding=utf-8
-import datetime
 import json
 import random
 import time
+from typing import Dict, List
 
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from utils import chores, reload_config, safe_append, merge_chores, colors, update
+from utils import config, reload_config, safe_append, merge_chores, colors, update, should_run_bi_weekly, \
+    should_run_quad_weekly
 
-SLACK_URL = 'https://hooks.slack.com/services/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'
-DEV_URL = 'https://hooks.slack.com/services/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'
-USERS = ('gemanley', 'jpbush', 'keane',)
-SHARED_CATEGORIES = ('Kitchen', 'General', 'Living-room')
-SECONDARY_SHARED_CATEGORIES = ('Bathroom',)
-SECONDARY_SHARED_USERS = ('keane', 'gemanley',)
+# Base URL for Slack incoming webhooks
+SLACK_URL = 'https://hooks.slack.com/services/{}'
 
 
-# SLACK_URL = DEV_URL
-
-
-def get_week():
-    return datetime.datetime.now().isocalendar()[1]
-
-
-def should_run_bi_weekly():
-    return get_week() % 2 == 1
-
-
-def should_run_quad_weekly():
-    return get_week() % 4 == 1
-
-
-def get_quote_of_the_day():
+def get_quote_of_the_day() -> None:
+    """
+    Fetch a memorable quote and post it to Slack
+    :return:
+    """
     r = requests.get('http://quotes.rest/qod.json?category=inspire').json()
     quote = r['contents']['quotes'][0]
     data = {
@@ -42,14 +28,23 @@ def get_quote_of_the_day():
     post_to_slack(data)
 
 
-def post_to_slack(data):
-    requests.post(SLACK_URL, data=json.dumps(data))
+def post_to_slack(data: Dict) -> None:
+    """
+    Handle the request to Slack
+    :param data: JSON-ready data
+    """
+    requests.post(SLACK_URL.format(config['slack_token']), data=json.dumps(data))
 
 
-def chores_to_slack(name, user_chores):
+def chores_to_slack(name: str, user_chores: Dict) -> None:
+    """
+    Format the prepared user chores for Slack
+    :param name: Name of these chores (weekly, bi-weekly, etc.)
+    :param user_chores: Dictionary of each user and their assigned chores
+    """
     for user in user_chores:
         data = {
-            'text': '{} chores for @{}'.format(name, user),
+            'text': '{} chores for <@{}>'.format(name, user),
             'attachments': [],
         }
         for group in user_chores[user]:
@@ -66,7 +61,13 @@ def chores_to_slack(name, user_chores):
         post_to_slack(data)
 
 
-def get_shared_chores(chores, chore_group):
+def get_shared_chores(chores: Dict, chore_group: List) -> List:
+    """
+    Find chores that are shared and should be assigned to a pool of several users.
+    :param chores: All chores
+    :param chore_group: List of the categories that should be shared
+    :return: A randomized list of chores to be assigned
+    """
     shared_chores = []
     for group in chores:
         if group in chore_group:
@@ -75,32 +76,50 @@ def get_shared_chores(chores, chore_group):
     return random.sample(shared_chores, len(shared_chores))
 
 
-def get_user_chores(chores):
+def get_user_chores(chores: Dict[str, str]) -> Dict[str, str]:
+    """
+    The meat of the program. Gets and assigns all the chores as desired.
+    :param chores: All chores to assign
+    :return: A dictionary of each user and their assigned chores
+    """
     user_chores = {}
-    for user in USERS:
-        user_chores[user] = {}
-    for i, chore in enumerate(get_shared_chores(chores, SHARED_CATEGORIES)):
-        safe_append(user_chores[USERS[i % len(USERS)]], chore[1], chore[0])
-    for i, chore in enumerate(get_shared_chores(chores, SECONDARY_SHARED_CATEGORIES)):
-        safe_append(user_chores[SECONDARY_SHARED_USERS[i % len(SECONDARY_SHARED_USERS)]], chore[1], chore[0])
+    # Prepare each user
+    for user in config['users']:
+        user_chores[user['name']] = {}
+    # Randomly assign chores between all users
+    for i, chore in enumerate(get_shared_chores(chores, config['shared_categories'])):
+        safe_append(user_chores[config['users'][i % len(config['users'])]['name']], chore[1], chore[0])
+    # Randomly assign chores between a secondary set of users
+    for i, chore in enumerate(get_shared_chores(chores, config['secondary_shared_categories'])):
+        safe_append(user_chores[config['secondary_shared_users'][i % len(config['secondary_shared_users'])]], chore[1],
+                    chore[0])
     for group in chores:
-        if group in SHARED_CATEGORIES:
+        # We've already assigned the shared chores- move on
+        if group in config['shared_categories']:
             pass
-        elif group in SECONDARY_SHARED_CATEGORIES:
-            for user in USERS:
-                if user not in SECONDARY_SHARED_USERS:
+        # Whoever's not in the shared group has these chores on their own,
+        # e.g. a second bathroom that's not shared
+        elif group in config['secondary_shared_categories']:
+            for user in config['users']:
+                if user not in config['secondary_shared_users']:
                     for chore in chores[group]:
-                        safe_append(user_chores[user], group, chore)
+                        safe_append(user_chores[user['name']], group, chore)
         else:
-            for user in USERS:
+            # Chores that need to go to every user individually
+            for user in config['users']:
                 for chore in chores[group]:
-                    safe_append(user_chores[user], group, chore)
+                    safe_append(user_chores[user['name']], group, chore)
     return user_chores
 
 
-def get_chores(period):
+def get_chores(period: str) -> Dict[str, str]:
+    """
+    Find chores for this time period.
+    :param period:
+    :return: Dictionary of chores for this time period.
+    """
     chores_dict = {}
-    for group in chores[period]:
+    for group in config[period]:
         chores_dict.update(group)
     return chores_dict
 
@@ -120,19 +139,24 @@ def weekly_clean():
 
 
 def credit_check():
-    for bill in chores['credit']:
-        if bill['credit'] - chores['dues'] <= 0:
+    """
+    Calculate whether any users have outstanding dues to pay the head of household, post to Slack.
+    """
+    for user in config['users']:
+        if user.get('credit') and user['credit'] - config['dues'] <= 0:
             data = {
-                'text': "@{}, you currently don't have enough credit"
-                " for the next rent payment. Please pay the minimum"
-                " amount to Keane as soon as possible. goo.gl/BKUN3b".format(
-                    bill['name']
-                )
+                'text': "<@{}>, you currently don't have enough credit"
+                        " for the next rent payment. Please pay the minimum"
+                        " amount to <@{}> as soon as possible. {}".format(user['name'], config['payto'],
+                                                                          config.get('payments', ''))
             }
             post_to_slack(data)
 
 
 def run_chores():
+    """
+    Decide which chores should be run, format them, and post to Slack.
+    """
     all_chores = []
     chore_string = ""
     if should_run_quad_weekly():
@@ -149,6 +173,7 @@ def run_chores():
 
 if __name__ == '__main__':
     reload_config()
+    weekly_clean()
     sched = BackgroundScheduler()
     sched.start()
     sched.add_job(update, trigger='cron', hour=6)
--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
+import datetime
 import subprocess
+from typing import Dict
 
 import yaml
 
-chores = {}
+config = {}
+# Define sidebar colors for each section in Slack.
 colors = {
     'Kitchen': 'a37718',
     'Bathroom': '18a39e',
@@ -14,24 +17,44 @@ colors = {
 
 
 def update():
+    """
+    Update from git
+    """
     subprocess.call(['git', 'pull', 'origin', 'master'])
     subprocess.call(['/srv/chores/env/bin/pip', 'install', '-r', 'requirements.txt', '--upgrade'])
     subprocess.call(['sudo', 'systemctl', 'restart', 'chores'])
 
 
-def reload_config():
-    chores.clear()
-    with open('chores.yml') as f:
-        chores.update(yaml.load(f))
+def reload_config() -> None:
+    """
+    Reload the config file.
+    """
+    config.clear()
+    try:
+        with open('chores.yml') as f:
+            config.update(yaml.load(f))
+    except FileNotFoundError:
+        print("`chores.yml` not found! Copy `example-chores.yml` for a starting point.")
 
 
-def safe_append(target, key, chore):
+def safe_append(target: Dict, key: str, chore: Dict) -> None:
+    """
+    Simply make sure dictionary keys exist before adding to them
+    :param target: Reference to dictionary
+    :param key: The key to create or update
+    :param chore: The chore to add to the dictionary
+    """
     if not target.get(key):
         target[key] = []
     target[key].append(chore)
 
 
-def merge_chores(user_chores):
+def merge_chores(user_chores: Dict) -> Dict:
+    """
+    Merge all chores into one mega dictionary.
+    :param user_chores: The user chores
+    :return: New dictionary
+    """
     all_chores = {}
     for category in user_chores:
         for user in category:
@@ -42,3 +65,15 @@ def merge_chores(user_chores):
                     all_chores[user][group] = []
                 all_chores[user][group].extend(category[user][group])
     return all_chores
+
+
+def get_week():
+    return datetime.datetime.now().isocalendar()[1]
+
+
+def should_run_bi_weekly():
+    return get_week() % 2 == 1
+
+
+def should_run_quad_weekly():
+    return get_week() % 4 == 1
